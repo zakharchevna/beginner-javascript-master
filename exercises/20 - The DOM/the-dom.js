329aa8f27fea6946997a8dec22cb15c4a68ee1e7
Olha Zakharchevna <zakharchevna@gmail.com>
2023-07-31 05:11:02 +0200

    dom practice window obj, document obj, window.navigator and selecting elements

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,19 @@
+// function init() {
+//   const p = document.querySelector('p');
+//   console.log(p);
+// }
+// document.addEventListener('DOMContentLoaded', init);
+
+const p = document.querySelector('p');
+console.log(p);
+
+const divs = document.querySelectorAll('div');
+console.log(divs);
+
+
+const item2 = document.querySelector('.item2');
+const item2Image = item2.querySelector('img');
+
+console.log(item2Image);
+
+
