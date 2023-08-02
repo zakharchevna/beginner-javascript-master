0bde2686f65f7d8a0ecb2f05208a51a6c5379348
Olha Zakharchevna <zakharchevna@gmail.com>
2023-08-02 08:59:43 +0200

    dom practice add, remove, toggle classes

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -4,19 +4,33 @@
 // }
 // document.addEventListener('DOMContentLoaded', init);
 
-const p = document.querySelector('p');
+// const p = document.querySelector('p');
 // console.log(p);
 
-const divs = document.querySelectorAll('div');
+// const divs = document.querySelectorAll('div');
 // console.log(divs);
 
-const heading = document.querySelector('h2');
-console.log(heading.textContent);
-console.log(heading.innerText);
+// const heading = document.querySelector('h2');
+// console.log(heading.textContent);
+// console.log(heading.innerText);
 
-const pizzaList = document.querySelector('.pizza');
-console.log(pizzaList.textContent);
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent);
 
 // pizzaList.textContent = `${pizzaList.textContent} 🍕`
 
-pizzaList.insertAdjacentText('beforeend', '🍕');
+// pizzaList.insertAdjacentText('', '🍕');
+
+const pic = document.querySelector('.nice');
+console.log(pic.classList);
+pic.classList.add('open');
+console.log(pic.classList);
+// pic.classList.add('round');
+// pic.classList.toggle('round');
+console.log(pic.classList);
+function toggleRound() {
+  pic.classList.toggle('round');
+}
+
+pic.addEventListener('click', toggleRound);
+
