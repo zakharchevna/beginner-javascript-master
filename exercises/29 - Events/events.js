63d7b23c895d5e85ae50ddeb1cb8c813f4f87679
Olha Zakharchevna <zakharchevna@gmail.com>
2023-08-03 18:24:53 +0200

    event-practice - Targets, Bubbling, Propagation and Capture

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -13,12 +13,44 @@ btn.removeEventListener('click', handleBtnClicked);
 const buyButtons = document.querySelectorAll('button.buy');
 
 
-function logClickedBtn(buyButton) {
-  buyButton.addEventListener('click', handleBtnClicked);
-}
-function removeClickedBtn(buyButton) {
-  buyButton.removeEventListener('click', handleBtnClicked);
-}
+// function logClickedBtn(buyButton) {
+//   buyButton.addEventListener('click', handleBtnClicked);
+// }
+// function removeClickedBtn(buyButton) {
+//   buyButton.removeEventListener('click', handleBtnClicked);
+// }
 
-buyButtons.forEach(logClickedBtn);
-buyButtons.forEach(removeClickedBtn);
+// buyButtons.forEach(logClickedBtn);
+// buyButtons.forEach(removeClickedBtn);
+function handleBuyButtonClick(event) {
+  console.log("You are buying it");
+  console.log(event.target.dataset.price);
+  // console.log(typeof event.target.dataset.price); //string
+  console.log(parseFloat(event.target.dataset.price));
+  // console.log(typeof parseFloat(event.target.dataset.price)); //number
+  const button = event.target;
+  console.log(button.textContent);
+  console.log(event.currentTarget);
+  console.log(event.target);
+  console.log(event.target === event.currentTarget); // true
+  // event.stopPropagation();
+};
+
+window.addEventListener('click', (event) => {
+  console.log('You have clicked the window');
+  console.log(event.target);
+  console.log(event.type);
+  event.stopPropagation();
+  console.log(event.bubbles);
+}), {capture: true};
+
+buyButtons.forEach(function(buyButton){
+  buyButton.addEventListener('click', handleBuyButtonClick);
+});
+
+const photoEl = document.querySelector(".photo");
+photoEl.addEventListener('mousemove', function(e){
+  console.log(e.currentTarget);
+  console.count(e.currentTarget);
+  console.log(this); 
+});
