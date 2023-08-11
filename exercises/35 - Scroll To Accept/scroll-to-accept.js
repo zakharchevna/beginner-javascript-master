71aba8089c31b6efd82c1d0b4b4354b9ba17222c
Olha Zakharchevna <zakharchevna@gmail.com>
2023-08-11 09:01:44 +0200

    scroll to accept practice

---
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -0,0 +1,39 @@
+function scrollToAccept() {
+  const terms = document.querySelector(".terms-and-conditions");
+  
+  if (!terms) {
+    return;
+  }
+
+  const watch = document.querySelector(".watch");
+  const button = document.querySelector(".accept");
+
+  function obCallBack(load) {
+    // console.log(load[0]);
+    // console.log(load[0].isIntersecting);
+    // console.log(load[0].intersectionRatio);
+    if(load[0].intersectionRatio === 1) {
+      button.disabled = false;
+      ob.unobserve(terms.lastElementChild)
+    } 
+    // else {
+    //   button.disabled = true;
+    // }
+  }
+
+  const ob = new IntersectionObserver(obCallBack, {
+    root: terms,
+    threshold: 1,
+  });
+
+//   ob.observe(terms.lastElementChild);
+
+//   function handleScrollTerms(event) {
+    // console.log(event.currentTarget.scrollTop);
+    // console.log(event.currentTarget.scrollHeight);
+//   }
+  
+//   terms.addEventListener("scroll", handleScrollTerms);
+// }
+
+// scrollToAccept();
