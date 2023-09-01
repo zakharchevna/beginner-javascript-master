51a9f18ba2f943f0a1a3112f72867b9e1653bfdb
Olha Zakharchevna <zakharchevna@gmail.com>
2023-09-01 09:52:48 +0200

    add deleteItem function and markAsComplete function

--- exercises/57 - Shopping List/shopping.js
+++ exercises/57 - Shopping List/shopping.js
@@ -1,14 +1,14 @@
 const shoppingForm = document.querySelector('.shopping');
 const list = document.querySelector('.list');
-const items = [];
+let items = [];
 
 
 
 function displayItems () {
   const html = items.map((item) => `<li class="shopping-item">
-  <input type="checkbox">
+  <input type="checkbox" value="${item.id}" ${ item.complete ? 'checked' : ''}>
   <span class="itemName">${item.name}</span>
-  <button aria-label="Remove ${item.name}">&times;</button>
+  <button aria-label="Remove ${item.name}" value="${item.id}">&times;</button>
   </li>`).join("");
   list.innerHTML = html;
 }
@@ -23,7 +23,6 @@ function handleSubmit(e) {
     complete: false,
   };
   items.push(item);
-  console.log(`there are ${items.length} in your state`);
   e.target.reset();
   displayItems()
   list.dispatchEvent(new CustomEvent('itemsUpdated'));
@@ -39,19 +38,37 @@ function restoreFromLocalStorage() {
   const lsItems = JSON.parse(localStorage.getItem('items'));
   if(lsItems.length) {
     items.push(...lsItems);
+    list.dispatchEvent(new CustomEvent('itemsUpdated'));
   }
-  list.dispatchEvent(new CustomEvent('itemsUpdated'));
-  console.log('restore data from local storage')
 }
 
+function deleteItem(id) {
+  console.log('deleting item', id)
+  items = items.filter((item) => item.id !== id);
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
 
+function markAsComplete(id) {
+  console.log('checked');
+  const refItem = items.find(item => item.id === id);
+  console.log(refItem)
+  refItem.complete = !refItem.complete;
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
 
 list.addEventListener('itemsUpdated', displayItems);
 list.addEventListener('itemsUpdated', mirrorToLocalStorage);
+list.addEventListener('click', function(e) {
+  const id = parseInt(e.target.value);
+  if(e.target.matches('button')) {
+    deleteItem(id);
+  }
+  if(e.target.matches('input[type="checkbox"]')) {
+    markAsComplete(id);
+  }
+})
 shoppingForm.addEventListener('submit', handleSubmit);
 
+
+
 restoreFromLocalStorage();
-
-
-const buttons = list.querySelectorAll('button');
-console.dir(buttons)
