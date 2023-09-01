0283f6f45eb60c0ed146f6755bca59b6c6351e18
Olha Zakharchevna <zakharchevna@gmail.com>
2023-09-01 08:46:38 +0200

    set items to local storage and get items from local storage

--- exercises/57 - Shopping List/shopping.js
+++ exercises/57 - Shopping List/shopping.js
@@ -3,6 +3,7 @@ const list = document.querySelector('.list');
 const items = [];
 
 
+
 function displayItems () {
   const html = items.map((item) => `<li class="shopping-item">
   <input type="checkbox">
@@ -25,10 +26,32 @@ function handleSubmit(e) {
   console.log(`there are ${items.length} in your state`);
   e.target.reset();
   displayItems()
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
+
+
+function mirrorToLocalStorage() {
+  localStorage.setItem('items', JSON.stringify(items));
+  console.info('saving items to local storage');
+}
+
+function restoreFromLocalStorage() {
+  const lsItems = JSON.parse(localStorage.getItem('items'));
+  if(lsItems.length) {
+    items.push(...lsItems);
+  }
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+  console.log('restore data from local storage')
 }
 
 
 
-
-
+list.addEventListener('itemsUpdated', displayItems);
+list.addEventListener('itemsUpdated', mirrorToLocalStorage);
 shoppingForm.addEventListener('submit', handleSubmit);
+
+restoreFromLocalStorage();
+
+
+const buttons = list.querySelectorAll('button');
+console.dir(buttons)
