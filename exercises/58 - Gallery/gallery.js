46bf8a33fa8e95f97c3abde61e6c2312559a66b7
Olha Zakharchevna <zakharchevna@gmail.com>
2023-09-02 13:14:18 +0200

    add showNextImage function, showPrevImage function, open modal, close modal, handle keyup function

--- exercises/58 - Gallery/gallery.js
+++ exercises/58 - Gallery/gallery.js
@@ -8,6 +8,7 @@ function Gallery(gallery) {
   const modal = document.querySelector('.modal');
   const prevButton = modal.querySelector('.prev');
   const nextButton = modal.querySelector('.next');
+  let currentImage;
 
   function showImage(el) {
     if(!el) {
@@ -17,10 +18,63 @@ function Gallery(gallery) {
     console.log(el);
     modal.querySelector('img').src = el.src;
     modal.querySelector('h2').textContent = el.title;
+    modal.querySelector('figure p').textContent = el.dataset.desctiption;
+    currentImage = el;
+    openModal();
+  }
+  function showNextImage() {
+    showImage(currentImage.nextElementSibling || gallery.firstElementChild);
   }
 
+  function showPrevImage() {
+    showImage(currentImage.previousElementSibling || gallery.lastElementChild);
+  }
 
-  images.forEach(image => image.addEventListener('click', e => showImage(e.currentTarget)));
+  function openModal() {
+    if(modal.matches('.open')) {
+      console.log('It is alredy opened');
+      return
+    }
+    modal.classList.add('open');
+    console.log('Opening modal');
+    window.addEventListener('keyup', handleKeyUp);
+    nextButton.addEventListener('click', showNextImage);
+    prevButton.addEventListener('click', showPrevImage);
+  }
+
+  function closeModal() {
+    modal.classList.remove('open');
+    window.removeEventListener('keyup', handleKeyUp);
+    nextButton.removeEventListener('click', showNextImage);
+    //TODO: add Event Listener for clicks and keyboard
+  }
+
+  function handleClickOutside(e) {
+    if (e.target === e.currentTarget) {
+      closeModal();
+    }
+  }
+
+  function handleKeyUp(e) {
+    console.log(e.key + ' -> closing modal')
+    if (e.key === 'Escape') return closeModal();
+    if(e.key === 'ArrowRight') return showNextImage();
+    if(e.key === 'ArrowLeft') return showPrevImage();
+  }
+
+  
+
+  //There are EventListeners 
+  images.forEach((image) => image.addEventListener('click', e => showImage(e.currentTarget)));
+  images.forEach((image) => {
+    image.addEventListener('keyup', (e) => {
+      if(e.key === 'Enter') {
+        showImage(e.currentTarget);
+      }
+    })
+  })
+  modal.addEventListener('click', handleClickOutside);
+  
 }
 
 const gallery1 = Gallery(document.querySelector('.gallery1'));
