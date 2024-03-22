a1a16d0b6ccb561d33fa8137624f77d141107d34
Olha Zakharchevna <zakharchevna@gmail.com>
2024-03-22 16:15:38 +0100

    add draw-recursion fn

--- exercises/73 - Async Typer/scripts.js
+++ exercises/73 - Async Typer/scripts.js
@@ -6,19 +6,38 @@ function getRandomBetween(min = 20, max = 150) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-async function draw(el) {
-  console.log(el);
-  const text = el.textContent;
-  let soFar = "";
+// async function draw(el) {
+//   console.log(el);
+//   const text = el.textContent;
+//   let soFar = "";
 
-  for(const letter of text) {
-    soFar += letter;
-    el.textContent = soFar;
-    const { typeMin, typeMax } = el.dataset;
+//   for(const letter of text) {
+//     soFar += letter;
+//     el.textContent = soFar;
+//     const { typeMin, typeMax } = el.dataset;
+//     const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+//     await wait(amountOfTimeToWait);
+//   }
+// }
+
+
+function drawRecursion(el) {
+  let index = 1;
+  const text = el.textContent;
+  const { typeMin, typeMax } = el.dataset;
+  
+  async function drawLetter() {
+    el.textContent = text.slice(0, index);
+    index += 1;
     const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
     await wait(amountOfTimeToWait);
+    
+    if(index <= text.length) {
+      drawLetter();
+    }
   }
+
+  drawLetter();
 }
 
-const els = document.querySelectorAll("[data-type]").forEach(draw);
-
+const els = document.querySelectorAll("[data-type]").forEach(drawRecursion);
