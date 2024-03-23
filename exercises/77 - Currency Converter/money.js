0c6c8cb9562a682a206ee8c9444fba260542699a
Olha Zakharchevna <zakharchevna@gmail.com>
2024-03-24 00:31:05 +0100

    Create Currency Converter

--- exercises/77 - Currency Converter/money.js
+++ exercises/77 - Currency Converter/money.js
@@ -32,3 +32,67 @@ const currencies = {
   ZAR: 'South African Rand',
   EUR: 'Euro',
 };
+
+const endpoint = "https://api.apilayer.com/exchangerates_data/latest";
+
+const fromInput = document.querySelector("[name='from-amount']");
+const fromSelect = document.querySelector("[name='from-currency']");
+const toSelect = document.querySelector("[name='to-currency']");
+const output = document.querySelector(".to-amount");
+const form = document.querySelector('.app form');
+
+
+const myHeaders = new Headers();
+myHeaders.append("apikey", "7Sen3aLSTjgAUDdeht0kesqul75JVZJO");
+
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+function generateOptions(options) {
+  return Object.entries(options)
+  .map(([currencyCode, currencyName]) =>
+    `<option value="${currencyCode}">${currencyCode} - ${currencyName}</option>`)
+    .join("");
+}
+
+const optionsHTML = generateOptions(currencies);
+
+fromSelect.innerHTML = optionsHTML;
+toSelect.innerHTML = optionsHTML;
+
+async function fetchRates(base = "USD", symbols = "EUR") {
+  const res = await fetch(`${endpoint}?symbols=${symbols}&base=${base}`, requestOptions);
+  const rates = await res.json()
+  .catch(error => console.log('error', error));
+  return rates;
+}
+
+async function convert(amount, from, to) {
+  const rates = await fetchRates(from, to);
+  const rate = rates.rates[to];
+  const convertedAmount = amount * rate;
+  console.log(`${amount} ${from} is ${convertedAmount} in ${to}`);
+  return convertedAmount;
+}
+
+async function handleInput(e) {
+  const rawAmount = await convert(
+    fromInput.value,
+    fromSelect.value,
+    toSelect.value
+  )
+  // output.textContent = formatCurrency(rawAmount, toSelect.value);
+  output.textContent = rawAmount.toFixed(2);
+}
+
+// function formatCurrency(amount, currency) {
+//   return Intl.NumberFormat("en-US", {
+//     style: 'currency',
+//     currency
+//   }).format(amount);
+// }
+
+form.addEventListener("input", handleInput);
