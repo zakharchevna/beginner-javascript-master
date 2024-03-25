780f8a6319e249d14d144da6953c764027d6abd7
Olha Zakharchevna <zakharchevna@gmail.com>
2024-03-25 15:38:04 +0100

    Generate fake data using faker npm

---
+++ exercises/82.1 - npm modules/index.js
@@ -0,0 +1,21 @@
+import wait from "waait";
+import { faker } from '@faker-js/faker';
+console.log("Test: it works");
+
+
+const fakeEmojiArr = Array.from({length: 10}, () => faker.internet.emoji());
+const fakePersons = Array.from({length: 10}, () => faker.person.fullName());
+const fakerData = document.querySelector(".faker-data");
+
+fakerData.innerHTML = `
+  <p><strong>Random emoji generated with faker npm:</strong> ${fakeEmojiArr.join("")}</p>
+  <p><strong>Random persons generated with faker npm:</strong> ${fakePersons.join(", ")}</p>
+`;
+
+async function go() {
+  console.log("Going");
+  await wait(2000);
+  console.log("Ending");
+}
+
+go();
