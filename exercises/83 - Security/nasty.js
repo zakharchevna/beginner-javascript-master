f668fa1664c8feb7e16f319cd4c8657371d4124a
Olha Zakharchevna <zakharchevna@gmail.com>
2024-03-28 17:05:46 +0100

    Practice domPurify npm

--- exercises/83 - Security/nasty.js
+++ exercises/83 - Security/nasty.js
@@ -4,7 +4,11 @@ const input = document.querySelector('[name="input"]');
 const output = document.querySelector('.output');
 const buttons = document.querySelectorAll('nav button');
 input.addEventListener('input', () => {
-  output.innerHTML = input.value.replace(/\n/g, '<br>');
+  const clean = sanitize(input.value, {
+    FORBID_ATTR: ['style', 'width', 'height'],
+    FORBID_TAGS: ['style']
+  })
+  output.innerHTML = clean.replace(/\n/g, '<br>');
 });
 
 // trigger an input even on page load
